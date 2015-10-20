42f953c86f1d53d412b080621cdbcd598bd39f87
basiabodurov <basiabodurov@gmail.com>
2015-10-20 11:54:15 -0700

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+var gulp = require('gulp');// Load Gulp!
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+gulp.task('scss', function () {
+	gulp.src('./scss/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+	.pipe(gulp.dest('./css'));
+});
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+
+	gulp.watch('./scss/**/*.scss', ['scss']);
+	gulp.watch(["*/**/*.html","css/*.css","js/*.js"]).on('change', browserSync.reload);
+});
